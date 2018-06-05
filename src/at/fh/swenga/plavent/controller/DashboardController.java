0472eafb6a11489c540ade789f0c8daf055cbd1e
Stefan Heider <stefan.heider@edu.fh-joanneum.at>
2018-06-05 17:01:15 +0200

    issue Nr 29: Dashboard show next upcoming happenings

--- src/at/fh/swenga/plavent/controller/DashboardController.java
+++ src/at/fh/swenga/plavent/controller/DashboardController.java
@@ -1,16 +1,20 @@
 package at.fh.swenga.plavent.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+import at.fh.swenga.plavent.model.Happening;
 import at.fh.swenga.plavent.repo.HappeningCategoryRepository;
+import at.fh.swenga.plavent.repo.HappeningRepository;
 import at.fh.swenga.plavent.repo.HappeningStatusRepository;
 import at.fh.swenga.plavent.repo.UserRepository;
 import at.fh.swenga.plavent.repo.UserRoleRepository;
@@ -26,6 +30,9 @@ public class DashboardController {
 	private UserRoleRepository userRoleRepository;
 
 	@Autowired
+	private HappeningRepository happeningRepository;
+	
+	@Autowired
 	private HappeningStatusRepository happeningStatusRepository;
 
 	@Autowired
@@ -37,11 +44,36 @@ public class DashboardController {
 
 	@Secured({ "ROLE_GUEST"})
 	@RequestMapping(value = { "dashboard" })
-	public String showDashboard(Model model) {
+	public String showDashboard(Model model,Authentication authentication) {
 		
-		//TODO: Show stuff for logged in user
-		// Show main screen (dashboard)
+		//TODO: Show stuff for logged in user	
+		
+		//URD 1.1.1.17 Guest see list of happenings where user is guest orderd by start date and start in the future
+		model.addAttribute("happeningsForGuestInFuture", this.getHappeningForGuestInFuture(authentication.getName()));
+		
+		
+		if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_HOST"))) {
+			//TODO: load stuff for host havera
+		}
+		
+		if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+			//TODO: load stuff for admin havera
+		}
+		
+		
 		return "dashboard";
+	}
+	
+	
+	/**
+	 * Return a list of happenings where given user is a guest and the happenings start in the future
+	 * @param username
+	 * @return
+	 */
+	public List<Happening> getHappeningForGuestInFuture(String username) {
+		List<Happening> happenings = happeningRepository.getHappeningForGuestInFuture(username);
+		return happenings;
+		
 	}
 }
 
--- src/at/fh/swenga/plavent/repo/HappeningRepository.java
+++ src/at/fh/swenga/plavent/repo/HappeningRepository.java
@@ -17,34 +17,38 @@ import at.fh.swenga.plavent.model.Happening;
 @Transactional(isolation = Isolation.READ_COMMITTED)
 public interface HappeningRepository extends JpaRepository<Happening, Integer> {
 
-
 	public List<Happening> findByHappeningId(int happeningID);
-	
+
 	public Page<Happening> findByHappeningName(String happeningName, Pageable pageable);
-	
-	//No pageination variante (e.g. for InitialSetupController)
+
+	// No pageination variante (e.g. for InitialSetupController)
 	public List<Happening> findByHappeningName(String happeningName);
-	
-	public Page<Happening> findByHappeningNameAndHappeningHostUsername(String happeningName,String username, Pageable pageable);
-	
-	
+
+	public Page<Happening> findByHappeningNameAndHappeningHostUsername(String happeningName, String username,
+			Pageable pageable);
+
 	public List<Happening> findByHappeningHostUsername(String username);
 
-	
-	@Query(value = "SELECT h " + 
-		   			"FROM Happening h " +
-		   			"WHERE h.happeningHost.username = :host " +
-		   			"  AND LOWER(h.happeningStatus.statusName) = LOWER('ACTIVE')", nativeQuery = false)
+	@Query(value = "SELECT h " +
+					"FROM Happening h " + 
+					"WHERE h.happeningHost.username = :host " + 
+					"  AND LOWER(h.happeningStatus.statusName) = LOWER('ACTIVE')", nativeQuery = false)
 	public Page<Happening> getActiveHappeningsForHost(@Param("host") String hostUsername, Pageable pageable);
-	
-	
+
+	@Query(value = "SELECT h.* " + 
+				   "FROM Happening h " + 
+				   "WHERE h.happeningID = (SELECT happeningID "  +
+				   "					    FROM HappeningTask " + 
+				   "						WHERE taskId = :taskId)", nativeQuery = true)
+	public Happening getHappeningForTask(@Param("taskId") int taskId);
+
 	@Query(value = "SELECT h.* " +
 					"FROM Happening h " +
-					"WHERE h.happeningID = (SELECT happeningID " + 
-					"					    FROM HappeningTask " +
-					"						WHERE taskId = :taskId)", nativeQuery = true)
-	public Happening getHappeningForTask(@Param("taskId") int taskId);
+					"INNER JOIN Guestlist gl ON(gl.happeningId = h.happeningId) " +
+					"INNER JOIN Status s ON(s.statusID = h.statusId) " +
+					"WHERE h.start > CURRENT_TIMESTAMP() " +
+					"  AND s.statusName = 'ACTIVE' " +
+					"  And (gl.username = :username OR h.username = :username)" + 
+					"ORDER BY h.start", nativeQuery = true)
+	public List<Happening> getHappeningForGuestInFuture(@Param("username") String username);
 }
-
-
-
